7f4e30430ea9d55e35be3380a46750050496775c
Frank Cheng <ufo22940268@gmail.com>
2021-08-12 09:47:24 +0800

    Resize png.

--- main.go
+++ main.go
@@ -14,4 +14,14 @@ func main() {
 	)
 
 	imaging.Save(dstImg, "dist/resized_nike.jpg")
+
+	src, _ = imaging.Open("asset/white_sign.png")
+	dstImg = imaging.Resize(
+		src,
+		200,
+		0,
+		imaging.Lanczos,
+	)
+
+	imaging.Save(dstImg, "dist/resized_white_sign.png")
 }
